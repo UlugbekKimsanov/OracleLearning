cfe026808bbdec7f8d89607a2160df85cab28d0b
UlugbekKimsanov <kimanov0717@gmail.com>
2026-01-30 21:06:02 +0500

    Install commit

---
+++ src/main/java/uz/edu/oracle-learning/OracleHrApiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package uz.edu.oraclehrapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleHrApiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleHrApiInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/edu/oraclehrapiinterface/OracleHrApiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.edu.oraclehrapiinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleHrApiInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
