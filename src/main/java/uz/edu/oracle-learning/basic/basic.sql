d80bf968f997428fdecf1098a1cd8257d5d1e52e
UlugbekKimsanov <kimanov0717@gmail.com>
2026-01-30 22:10:27 +0500

    Basic sql queries

---
+++ src/main/java/uz/edu/oracle-learning/basic/basic.sql
@@ -0,0 +1,11 @@
+
+create table customer(
+    cust_id number(8),
+    cust_name varchar2(100),
+    mobile number(10),
+    dob date,
+    city varchar2(100),
+    email_id varchar2(100)
+);
+
+select * from customer;
