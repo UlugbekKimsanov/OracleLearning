1cb2b13ff9fa0c434921b958bc2700cd812fa724
UlugbekKimsanov <kimanov0717@gmail.com>
2026-02-02 22:38:03 +0500

    Qury types

--- src/main/java/uz/edu/oracle-learning/basic/sql_query_types/basic.sql
+++ src/main/java/uz/edu/oracle-learning/basic/sql_query_types/basic.sql
@@ -16,5 +16,7 @@ insert into customer values (2, 'aasdasd', 12901290, to_date('08/23/2026','mm/dd
 insert into customer (cust_id,cust_name, mobile, city,email_id)
 values (3, 'aasdasd', 12901290, 'asdasgmailcom','sdfsdfsdf');
 
+alter table customer modify cust_id number(10);
+
 
 
